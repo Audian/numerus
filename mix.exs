6ef1907e2d2599652a12773c76440b4a6fa98891
Pramod Venugopal <pramodv@audian.com>
2024-11-05 10:31:39 -0800

    Update area codes
    - Added missing area codes for US and Canada
    - Bump version to 0.4.2

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Numerus.MixProject do
   def project do
     [
       app: :numerus,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
