7c9507007c0abe3fa5f24bbb467a5b3634f2725b
Pramod Venugopal <pramodv@audian.com>
2024-11-05 10:39:37 -0800

    Added upcoming area code changes and expansions (#13)
    - Added upcoming area code changes and expansions.
    - Bump version to 0.4.3

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Numerus.MixProject do
   def project do
     [
       app: :numerus,
-      version: "0.4.2",
+      version: "0.4.3",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
